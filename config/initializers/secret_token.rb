9843ea98799793f760c97e8883bcf300aaadab60
anuccio <alex-nuccio@uiowa.edu>
2013-04-06 11:03:20 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProfMash::Application.config.secret_token = '78f44078f53a4fc86c4403b7dc65286eafb0feb53977583914983f4b853a9fa60c7d6e1e8dc11a515b9a0c571e527f67a630a82177a4eec17e4c53fd67f86d80'
