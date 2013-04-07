9c15996fba64bd70bfe2aad97e17b0650f3bd861
Brian Chau <brian-chau@uiowa.edu>
2013-04-06 20:30:56 -0500

    voting page user interface done

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,13 @@
 class VotesController < ApplicationController
   def index
 	@random = Professor.all(:order=>'RANDOM()', :limit => 2)
+
+  end	
+
+  def show
+	flash[:notice] = "You voted for #{Professor.find(params[:id]).first_name}  #{Professor.find(params[:id]).last_name}." 
+	flash.keep
+	redirect_to votes_path
   end
 
 end
