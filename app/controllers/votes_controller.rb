bab428b70dc95ddbe24952785f75005754f7e8ce
Brian Chau <brian-chau@uiowa.edu>
2013-04-06 21:05:24 -0500

    more stuff on the voting page.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,11 +1,16 @@
 class VotesController < ApplicationController
   def index
-	@random = Professor.all(:order=>'RANDOM()', :limit => 2)
+	@random = Professor.find(:all,:order=>'RANDOM()', :limit => 2)
+	@question = Course.find(:all,:order=>'RANDOM()', :limit => 1)
 
   end	
 
   def show
-	flash[:notice] = "You voted for #{Professor.find(params[:id]).first_name}  #{Professor.find(params[:id]).last_name}." 
+	if(params[:id] != -1)
+		flash[:notice] = "You skipped the previous question."
+	#elsif(params[:id] != -1)
+	#	flash[:notice] = "You voted for #{Professor.find(params[:id]).first_name}  #{Professor.find(params[:id]).last_name}." 
+	end
 	flash.keep
 	redirect_to votes_path
   end
