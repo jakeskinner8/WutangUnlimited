11d6458c8ecd92d61b4e9adee7fba949e16ede81
ankittrivedi <ankit-trivedi@uiowa.edu>
2013-04-11 16:24:18 -0700

    professorcontrollerspec

--- spec/controllers/professors_controller_spec.rb
+++ spec/controllers/professors_controller_spec.rb
@@ -1,2 +1,20 @@
 require 'spec_helper'
 
+describe ProfessorsController do
+  describe 'view page for professor bios' do
+    it 'I am on the teachers bio page , it should be loaded' do
+      mock = mock('CreateProfessor')
+      Professor.should_receive(:all).and_return(mock)
+      get :index
+      response.should be_success
+    end
+    it 'I click on professor "onef onel"' do
+     mock = mock('CreateProfessor')
+     Professor.should_receive(:all).and_return(mock)
+     Professor.should_receive(:find).with('3').and_return(mock)
+     get :show, {:id => '3'}
+     redirect_to(professor_path)
+    end
+end
+end
+
