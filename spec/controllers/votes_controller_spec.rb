d845b9d89b47f7338cfb59e32722a1471ada3566
jakeskinner8 <jacob-skinner@uiowa.edu>
2013-04-11 16:30:00 -0700

    rspec testing

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,2 +1,12 @@
 require 'spec_helper'
 
+describe VotesController do
+  describe 'Trying to vote without being logged in' do
+    it 'should redirect home page' do
+	visit "/votes"
+	page.should have_content "Teacher of The Month"
+	#response.should redirect_to(home_show_path)
+      
+    end
+  end
+end
