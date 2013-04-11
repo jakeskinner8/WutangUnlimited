24d9bef32d9926faa330767ed1a7146ee0f24c47
ankittrivedi <ankit-trivedi@uiowa.edu>
2013-04-11 16:50:32 -0700

    professorspecs

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -4,13 +4,8 @@ require 'spec_helper'
 describe Professor do
   describe 'Get random professors' do
     it 'should search for random professor' do
-      professor = mock('Movie', :director => 'Jacob Skinner')
-      Movie.should_receive(:find).with('1').and_return(movie)
-      fake_results = [mock('Movie'), mock('Movie')]
-      Movie.should_receive(:find_all_by_director).with('Jacob Skinner').and_return(fake_results)
-      Movie.movies_with_current_director('1')
+      professor = mock('Professor')
+      Professor.should_receive(:find, :all, :order=>'RANDOM()', :limit => 2).and_return(professor)
     end
    end
 end
-		@random = Professor.find(:all,:order=>'RANDOM()', :limit => 2)
-		@question = Course.find(:all,:order=>'RANDOM()', :limit => 1)
