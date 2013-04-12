ed6649026b7797f6e4be4a97a01941e7fb1ab62b
jakeskinner8 <jacob-skinner@uiowa.edu>
2013-04-11 17:38:06 -0700

    rspec

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -1,11 +1,2 @@
 require 'spec_helper'
 
-
-describe Professor do
-  describe 'Get random professors' do
-    it 'should search for random professor' do
-      professor = mock('Professor')
-      Professor.should_receive(:find, :all, :order=>'RANDOM()', :limit => 2).and_return(professor)
-    end
-   end
-end
