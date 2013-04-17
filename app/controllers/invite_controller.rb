816e6a486af044a8387217a4e7f3ce380976579c
Brian Chau <brian-chau@uiowa.edu>
2013-04-16 21:45:22 -0500

    only allows users to invite friends if they are signed in

---
+++ app/controllers/invite_controller.rb
@@ -0,0 +1,17 @@
+class InviteController < ApplicationController
+  before_filter :set_current_user
+  def set_current_user
+    @current_use = user_signed_in? 
+    if(!@current_use)
+	flash[:notice] = "You must be logged in to view this page."
+    end
+    redirect_to '/home' and return unless @current_use
+  end 
+  def sendinvite
+	@email = params[:email]["email"].split(",").each do |email|
+	  MyMailer.welcome(email).deliver
+	end
+	flash[:notice] = "Invite(s) Sent!"
+	redirect_to "/home"
+  end
+end
