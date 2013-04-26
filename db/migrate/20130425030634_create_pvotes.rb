d3c93e0c92cd8bd525250438daf34cc435942fd7
Brian Chau <brian-chau@uiowa.edu>
2013-04-25 21:06:00 -0700

    test

--- db/migrate/20130425030634_create_pvotes.rb
+++ db/migrate/20130425030634_create_pvotes.rb
@@ -1,8 +1,8 @@
 class CreatePvotes < ActiveRecord::Migration
   def change
     create_table :pvotes do |t|
-      t.references 'professor'
-      t.references 'pquestion'
+      t.references 'Professor'
+      t.references 'Pquestion'
       t.integer :wins 		, :default => 0
       t.integer :appearances	, :default => 0
       t.timestamps
