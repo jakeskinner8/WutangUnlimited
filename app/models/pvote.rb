4f7630c5e49f537cec1e2c1be3e5bfb40ffc12b2
jakeskinner8 <jacob-skinner@uiowa.edu>
2013-04-27 19:07:41 -0700

    win percentage

--- app/models/pvote.rb
+++ app/models/pvote.rb
@@ -2,5 +2,5 @@ class Pvote < ActiveRecord::Base
   belongs_to :Pquestion
   belongs_to :Professor
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :professor_id, :pquestion_id, :wins, :appearances
+  attr_accessible :professor_id, :pquestion_id, :wins, :appearances,:winpercentage
 end
