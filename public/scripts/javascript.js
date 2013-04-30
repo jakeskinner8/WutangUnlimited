140a7ca88c8bdeb5acebc5ff7b595059b2701376
Brian Chau <brian-chau@uiowa.edu>
2013-04-29 17:25:31 -0700

    test

---
+++ public/scripts/javascript.js
@@ -0,0 +1,22 @@
+function leftArrowPressed() {
+  document.getElementById('A').click();
+}
+
+function rightArrowPressed() {
+  document.getElementById('B').click();
+}
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    switch (evt.keyCode) {
+        case 37:
+            leftArrowPressed();
+            break;
+        case 39:
+            rightArrowPressed();
+            break;
+    }
+
+};
+
+
