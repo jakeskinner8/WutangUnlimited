01edf98bd91a1f49b85f9ba04904634cfd980c0b
Brian Chau <brian-chau@uiowa.edu>
2013-04-29 19:15:42 -0700

    test

--- public/scripts/javascript.js
+++ public/scripts/javascript.js
@@ -6,6 +6,9 @@ function rightArrowPressed() {
   document.getElementById('B').click();
 }
 
+function sPressed(){
+  document.getElementById('Skip').click();
+}
 document.onkeydown = function(evt) {
     evt = evt || window.event;
     switch (evt.keyCode) {
@@ -15,6 +18,10 @@ document.onkeydown = function(evt) {
         case 39:
             rightArrowPressed();
             break;
+        case 83:
+	    sPressed();
+	    break;
+
     }
 };
 
