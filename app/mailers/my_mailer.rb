a2c0ee515362b5bd8da7a30e0daf4e74686a06e1
Brian Chau <brian-chau@uiowa.edu>
2013-05-03 18:27:41 -0700

    test

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -6,8 +6,5 @@ class MyMailer < ActionMailer::Base
     mail to: email, subject: "You have been invited to join ProfMash"
   end
   def reset(email)
-    @sender = "profmash@hotmail.com"
-    @email = email
-    mail to: email, subject: "reset password instructions"
   end
 end
