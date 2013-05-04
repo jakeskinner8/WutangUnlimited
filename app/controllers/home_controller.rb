9cb8698c5f166bd27dfb10c9233fc251fff232b0
Brian Chau <brian-chau@uiowa.edu>
2013-05-03 19:02:15 -0700

    tes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -25,9 +25,6 @@ class HomeController < ApplicationController
   end
 
   def reset
-  	@email = params[:email]["email"]
-	MyMailer.reset(@email).deliver
-	
   end
   def question
 	@results = Userpollresult.all
